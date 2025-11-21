42a7f87a5261b7f3c0523a742519caa096c9e221
Piyush Goswami <piyush@Piyushs-MacBook-Air.local>
2025-11-21 18:19:03 +0530

    add explicit callback url

--- server.js
+++ server.js
@@ -68,7 +68,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback',
+    proxy: true
 },
     function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
